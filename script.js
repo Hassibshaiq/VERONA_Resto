6ebe48fcbc05a6c93c2a2a7ad55db1138dc641ef
Hassibshaiq <121828483+Hassibshaiq@users.noreply.github.com>
2023-01-24 12:30:14 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({
+  reset: true,
+  useDelay: 'always',
+  delay: 200
+});
+
+sr.reveal('#drinks');
+sr.reveal('#bar');
+sr.reveal('#foto');
+sr.reveal('#contact');
+sr.reveal('#footer');
